330e35882f8575cd94a53117b3d2300405e0f065
Anna <akozorkova@gmail.com>
2022-10-13 17:10:26 +0000

    Customize products in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'image',
+    )
+
+admin.site.register(Product, ProductAdmin)
