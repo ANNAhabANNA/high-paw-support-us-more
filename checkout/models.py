b6f8cbeef77ee99df53f780aad1f4a053e897017
Anna <akozorkova@gmail.com>
2022-11-24 21:46:01 +0000

    Create checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 from products.models import Product
 
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -64,4 +63,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'{self.product.name} on order {self.order.order_number}'
