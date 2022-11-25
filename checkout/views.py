6f603affa49a20ad50b5919e40dd41e34425ee6b
Anna <akozorkova@gmail.com>
2022-11-25 23:07:09 +0000

    Amend checkout template for improved UI

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
+from .forms import OrderForm
 def review_checkout(request):
     """ 
     A view that renders the checkout contents page
     """
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
