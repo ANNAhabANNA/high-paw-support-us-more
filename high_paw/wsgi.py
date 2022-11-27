5f11299778328a8c2ee371cfdfc36a990926fe0a
Anna <akozorkova@gmail.com>
2022-11-27 08:53:30 +0000

    Deployment

--- high_paw/wsgi.py
+++ high_paw/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'high_paw.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'high_paw.settings')
 
 application = get_wsgi_application()
