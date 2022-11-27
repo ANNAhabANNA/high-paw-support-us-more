4a39698f42602a24b13f97d1f003a99b6cb41a44
Anna <akozorkova@gmail.com>
2022-11-27 05:45:18 +0000

    Configure heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn high_paw.wsgi:application
--- high_paw/settings.py
+++ high_paw/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-1_)f=2n6e-t=0de-n4gxlag+bazbnjt5&9w4@(2n1(q=md!0l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['high-paw.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,16 +109,20 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'high_paw.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
