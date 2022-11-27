f1ad11bf5aa7d92daf3a40c19b2209fda6eff50f
Anna <akozorkova@gmail.com>
2022-11-27 07:46:12 +0000

    Depployment

--- high_paw/settings.py
+++ high_paw/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_)f=2n6e-t=0de-n4gxlag+bazbnjt5&9w4@(2n1(q=md!0l@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['high-paw.herokuapp.com', 'localhost']
 
