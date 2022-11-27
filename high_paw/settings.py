736e5e2d44be2ca12819c33a3823a602c0834f19
Anna <akozorkova@gmail.com>
2022-11-27 07:55:47 +0000

    Deployment

--- high_paw/settings.py
+++ high_paw/settings.py
@@ -14,17 +14,17 @@ import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', ')5oudm3^v+u2*-=5n(bpl!-(y@dl5mxiogrw4+xuir962ahmz$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['high-paw.herokuapp.com', 'localhost']
 
