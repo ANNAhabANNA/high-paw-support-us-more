f5739304d669db900e9c1a5ef7a568e9bb5b644e
Anna <akozorkova@gmail.com>
2022-11-27 11:39:58 +0000

    Deployment

--- high_paw/settings.py
+++ high_paw/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')5oudm3^v+u2*-=5n(bpl!-(y@dl5mxiogrw4+xuir962ahmz$')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
