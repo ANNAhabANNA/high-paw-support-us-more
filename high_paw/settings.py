bd18eab07ee6465c2ef55cfe9d3ff11ca89a4d32
Anna <akozorkova@gmail.com>
2022-11-27 09:38:56 +0000

    Deplyment

--- high_paw/settings.py
+++ high_paw/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', ')5oudm3^v+u2*-=5n(bpl!-(y@dl5mxiogrw4+xuir962ahmz$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['high-paw.herokuapp.com', 'localhost']
 
